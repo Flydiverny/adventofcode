c98b5f9def25779f56e8e8eef04c43e7cde1266b
Markus Maga <markus.maga@jayway.com>
2017-12-16 03:40:36 +0100

    Allow passing input to command to override data file

--- runner.js
+++ runner.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 if (process.argv.length <= 2) {
-  console.log('Usage: runner.js <day> [part]');
+  console.log('Usage: runner.js <day> [part] [input]');
   process.exit(-1);
 }
 
 let day = process.argv[2];
 const part = process.argv[3] || 'a';
+const input = process.argv[4];
 
 const solution = require(`./${day}${part}`);
 const readFile = require('./util/readFile');
 
-readFile(`input/${day}.dat`)
+const resolve = input ? Promise.resolve(input) : readFile(`input/${day}.dat`);
+
+resolve
   .then(solution)
   .then(console.log);
