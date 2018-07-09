1fe11ad792e6008aaedf1db851282c0b1cb499ae
Markus Maga <markus.maga@jayway.com>
2018-07-10 01:25:56 +0200

    refactor runner some because why not

--- runner.js
+++ runner.js
@@ -1,33 +1,55 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
-const Promise = require('bluebird');
+const Promise = require("bluebird");
+const meow = require("meow");
+const readFile = require("./util/readFile");
+const readInput = require("./util/readInput");
 
-if (process.argv.length <= 2) {
-  console.log('Usage: runner.js <day> [part] [input]');
-  process.exit(-1);
-}
+const cli = meow(
+  `
+  Usage
+    $ runner <day> [part]
 
-let day = process.argv[2];
-const part = process.argv[3] || 'a';
-const input = process.argv[4];
+  Options
+    --input, -i  Run using test input
 
-const solution = require(`./${day}${part}`);
-const readFile = require('./util/readFile');
-const readInput = require('./util/readInput');
+  Examples
+    $ runner 1 a
+`,
+  {
+    flags: {
+      input: {
+        type: "string",
+        alias: "i"
+      }
+    }
+  }
+);
 
-const pipePromise = readInput
-  .then((chunks) => chunks[0].trim())
-  .catch(() => {})
+const foo = ([day, part = "a"], { input }) => {
+  if (!day) {
+    console.log("Day must be specified");
+    process.exit(-1);
+  }
 
-const argPromise = Promise.resolve(input);
+  const solution = require(`./solutions/${day}${part}`);
 
-const filePromise = readFile(`input/${day}.dat`);
+  const pipePromise = readInput
+    .then(chunks => chunks[0].trim())
+    .catch(() => {});
 
-Promise.all([pipePromise, argPromise, filePromise])
-  .then(([pipe, arg, file]) => {
-    return pipe || arg || file;
-  })
-  .then(solution)
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => process.exit());
+  const inputPromise = input
+    ? Promise.resolve(input)
+    : readFile(`input/${day}.dat`);
+
+  Promise.all([pipePromise, inputPromise])
+    .then(([pipe, input]) => {
+      return pipe || input;
+    })
+    .then(solution)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => process.exit());
+};
+
+foo(cli.input, cli.flags);
