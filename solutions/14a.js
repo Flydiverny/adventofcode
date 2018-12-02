8a76bb46cb7c0a6b7c77ab09598e544847aa28ba
Markus Maga <markus.maga@jayway.com>
2018-12-02 17:54:42 +0100

    wip? idk

---
+++ solutions/14a.js
@@ -0,0 +1,20 @@
+const tie = require('./10b');
+const _ = require('lodash');
+
+module.exports = (input) => {
+  const hashTable = [...Array(128)].map((v, index) => tie(`${input}-${index}`))
+    .map((hash) => {
+      const binary = [];
+      for (let i = 0; i < hash.length-1; i += 2) {
+        const int = parseInt(hash.substr(i, 2), 16)
+        binary.push(_.pad(int.toString(2), 8, "0"))
+      }
+
+      return binary.join('');
+    })
+    .map((binary) => _.filter(binary, (c) => c === '1').length)
+    .reduce((t, c) => t + c)
+
+
+  return hashTable;
+}
