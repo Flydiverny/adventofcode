681a0659c8a3ece1a7296e6f52b921bb03127481
Markus Maga <markus@maga.se>
2019-12-04 00:35:31 +0100

    chore: setup

---
+++ runner.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+const meow = require("meow");
+const readFile = require("./util/readFile");
+const readInput = require("./util/readInput");
+
+const cli = meow(
+  `
+  Usage
+    $ runner <day> [part]
+
+  Options
+    --input, -i  Run using test input
+
+  Examples
+    $ runner 1 a
+`,
+  {
+    flags: {
+      input: {
+        type: "string",
+        alias: "i"
+      }
+    }
+  }
+);
+
+const foo = ([day, part = "a"], { input }) => {
+  if (!day) {
+    console.log("Day must be specified");
+    process.exit(-1);
+  }
+
+  const solution = require(`./solutions/${day}${part}`);
+
+  const pipePromise = readInput
+    .then(chunks => chunks[0].trim())
+    .catch(() => {});
+
+  const inputPromise = input
+    ? Promise.resolve(input)
+    : readFile(`input/${day}.dat`);
+
+  Promise.all([pipePromise, inputPromise])
+    .then(([pipe, input]) => {
+      return pipe || input;
+    })
+    .then(solution)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => process.exit());
+};
+
+foo(cli.input, cli.flags);
---
+++ util/readInput.js
@@ -0,0 +1,24 @@
+const Promise = require('bluebird');
+
+const inputPromise = new Promise((resolve, reject) => {
+  if (process.stdin.isTTY) {
+    return reject();
+  }
+
+  const stdin = process.stdin;
+  const stdout = process.stdout;
+  const inputChunks = [];
+
+  stdin.resume();
+  stdin.setEncoding('utf8');
+  stdin.on('data', (chunk) => {
+    inputChunks.push(chunk);
+  });
+
+  stdin.on('end', () => {
+    resolve(inputChunks);
+  });
+});
+
+
+module.exports = inputPromise;
