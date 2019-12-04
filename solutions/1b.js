e908f504e6032e206ac8fd5a2273d1b756d9cb63
Markus Maga <markus@maga.se>
2019-12-04 01:32:55 +0100

    feat: 1b

---
+++ solutions/1b.js
@@ -0,0 +1,13 @@
+import R from "ramda";
+
+const calculateFuel = mass => {
+  const fuel = Math.floor(mass / 3) - 2;
+  if (fuel <= 0) {
+    return 0;
+  }
+
+  return fuel + calculateFuel(fuel);
+};
+
+export default rawData =>
+  R.pipe(R.map(parseInt), R.map(calculateFuel), R.sum)(rawData);
